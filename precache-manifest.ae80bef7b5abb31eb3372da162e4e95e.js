ab23fe054171eebc9f03de3dd518965c41b86a8a
DHKIM <iedcon12@gmail.com>
2020-04-15 18:45:06 +0900

    Updates

---
+++ precache-manifest.ae80bef7b5abb31eb3372da162e4e95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188ad28f8b7bfff7e51411eec8d8b313",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1d9c9c609ccd91e35e68",
+    "url": "/movie_app/static/css/main.e614ebde.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9c9c609ccd91e35e68",
+    "url": "/movie_app/static/js/main.0ee35e86.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
